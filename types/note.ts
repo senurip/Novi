e0fc8ab25b46bad8676d665ad6177ece091b6cbb
senurip <senurirajapaksha1234@gmail.com>
2025-09-22 12:28:30 +0530

    Update data

--- types/note.ts
+++ types/note.ts
@@ -1,30 +1,45 @@
+// // export interface Note {
+// //   id?: string;
+// //   userId: string;          // Owner's UID
+// //   title: string;
+// //   content: string;
+// //   category?: string;
+// //   imageUrl?: string;
+// //   important?: boolean;
+// //   reminderTime?: string;   // e.g., "2025-09-07T10:00"
+// //   createdAt?: Date;
+// // }
+
+
 // export interface Note {
-//   id?: string;
-//   userId: string;          // Owner's UID
+//   id: string;
 //   title: string;
 //   content: string;
-//   category?: string;
-//   imageUrl?: string;
-//   important?: boolean;
-//   reminderTime?: string;   // e.g., "2025-09-07T10:00"
-//   createdAt?: Date;
+//   category: string;
+  
+//   // Store URLs (or null if not uploaded)
+//   //file?: string | null;
+//   audio?: string | null;
+
+//   imageUrl?: string | null;
+//   videoUrl?: string | null;
+//   fileUrl?: string | null;
+  
+  
+//   createdAt?: { seconds: number; nanoseconds?: number } | Date;
 // }
 
 
 export interface Note {
   id: string;
+  userId: string;   // who owns the note
   title: string;
   content: string;
   category: string;
-  
-  // Store URLs (or null if not uploaded)
-  //file?: string | null;
-  audio?: string | null;
 
-  imageUrl?: string | null;
-  videoUrl?: string | null;
-  fileUrl?: string | null;
-  
-  
+  imageUri?: string | null;  // 🔑 store local/file/URI string
+  videoUri?: string | null;
+  fileUri?: string | null;
+
   createdAt?: { seconds: number; nanoseconds?: number } | Date;
 }
